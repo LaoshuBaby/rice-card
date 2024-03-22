8e6def27c73991f1fd86cff90daba214570b9d06
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-03-22 19:50:14 +0800

    feat: modify position and add useful func

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,23 @@
 import json
 from PIL import Image, ImageDraw, ImageFont
 
+MAGIC_SYMBOL = {"REPO_URL": "get_repo_metadata"}
+
+
+def get_repo_metadata(repo_owner=None, repo_name=None, repo_url=None):
+    # 如果repo_owner和repo_name均不为空，则构建repo_url
+    if repo_owner and repo_name:
+        repo_url = "https://github.com/" + f"{repo_owner}/{repo_name}"
+    else:
+        repo_url = repo_url
+    return repo_url
+
+
+def get_colour(colour_name: str) -> str:
+    palette = {"python": "#66CCFF", "html": "#114514"}
+    return palette[colour_name]
+
+
 def create_open_graph_card(text_content, config_path, output_path):
     with open(config_path, "r") as f:
         config = json.load(f)
@@ -8,39 +25,58 @@ def create_open_graph_card(text_content, config_path, output_path):
     card_width = config["card"]["width"]
     card_height = config["card"]["height"]
     bg_color = tuple(config["card"]["bg_color"])
-    python_color = "#66CCFF"  # 定义python颜色
     card_image = Image.new("RGB", (card_width, card_height), bg_color)
     draw = ImageDraw.Draw(card_image)
 
     for element in config["elements"]:
         if element["type"] == "text_box":
+            text_pos = element["position"]
             text_content_key = element["content_key"]
             font = ImageFont.truetype(
                 element["font"]["name"], element["font"]["size"]
             )
             text = text_content[text_content_key]
-            text_pos = element["position"]
-            draw.text(text_pos, text, fill=tuple(element["font"]["color"]), font=font)
+            for magic_word in MAGIC_SYMBOL:
+                magic_word_key = "{{" + magic_word + "}}"
+                if magic_word_key in text:
+                    text = text.replace(
+                        magic_word_key,
+                        get_repo_metadata(
+                            repo_owner="laoshubaby", repo_name="rice-card"
+                        ),
+                    )
+            draw.text(
+                text_pos, text, fill=tuple(element["font"]["color"]), font=font
+            )
         elif element["type"] == "image":
-            img = Image.open(element["path"])
-            img = img.resize((element["width"], element["height"]))
+            img = Image.open(element["path"]).convert("RGBA")
+            img = img.resize(
+                (element["width"], element["height"]), Image.Resampling.LANCZOS
+            )
             img_pos = element["position"]
-            card_image.paste(img, img_pos)
+            # 假设background是您要将图片合并到的背景Image对象
+            # 确保背景图片也是RGBA模式，以支持透明度
+            card_image.paste(img, img_pos, img)
         elif element["type"] == "polygon":
             points = element["points"]
-            fill_color = python_color if element["fill"] == "python" else element["fill"]
+            fill_color = (
+                get_colour("python")
+                if element["fill"] == "python"
+                else element["fill"]
+            )
             draw.polygon(points, fill=fill_color)
 
     card_image.save(output_path)
 
+
 # 使用示例
 text_content = {
     "title": "肉骨茶——新加坡特色美食",
-    "desc": "在台灣也可能被寫作肉骨茶",
-    "extra_info": "享受美味的同时，不要忘记文化的意义。"
+    "desc": "在台灣也可能被寫作肉骨茶\n享受美味的同时，不要忘记文化的意义。",
+    "meta_link": "{{REPO_URL}}",
 }
 create_open_graph_card(
     text_content=text_content,
-    config_path="config.json",
+    config_path="../template/github_repo.json",
     output_path="preview_card.png",
 )
