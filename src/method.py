b44eebb3ae50c7af75995024f8b37e1216420e73
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-03-23 20:45:19 +0800

    feat: more common variables

--- src/method.py
+++ src/method.py
@@ -1,25 +1,59 @@
+from typing import List, Optional
 import requests
 import re
 
-def get_repo_stars(repo_owner=None, repo_name=None, repo_url=None):
-    print(repo_url)
+def get_repo_stars(repo_owner=None, repo_name=None, repo_url=None)->Optional[List[int]]:
     repo_url = get_repo_url(repo_owner, repo_name, repo_url)
     response = requests.get(repo_url)
-    # print(response.text)
 
     if response.status_code == 200:
         html_content = response.text
-        # return response.json()["stargazers_count"]
         pattern = r'<[^>]+id="repo-stars-counter-star"[^>]*>(.*?)</[^>]+>' 
         matches = re.findall(pattern, html_content, re.DOTALL)
-        print(matches)
+        # print(matches)
+        return matches
     else:
-        return 0
+        return None
 
-def get_repo_url(repo_owner=None, repo_name=None, repo_url=None):
+
+def get_repo_forks(repo_owner=None, repo_name=None, repo_url=None)->Optional[List[int]]:
+    repo_url = get_repo_url(repo_owner, repo_name, repo_url)
+    response = requests.get(repo_url)
+
+    if response.status_code == 200:
+        html_content = response.text
+        pattern = r'<[^>]+id="repo-network-counter"[^>]*>(.*?)</[^>]+>' 
+        matches = re.findall(pattern, html_content, re.DOTALL)
+        # print(matches)
+        return matches
+    else:
+        return None
+
+
+def get_repo_watchers(repo_owner=None, repo_name=None, repo_url=None)->Optional[List[int]]:
+    repo_url = get_repo_url(repo_owner, repo_name, repo_url)
+    response = requests.get(repo_url)
+
+    if response.status_code == 200:
+        html_content = response.text
+        pattern = r'<[^>]+id="repo-notifications-counter"[^>]*>(.*?)</[^>]+>'
+        matches = re.findall(pattern, html_content, re.DOTALL)
+        # print(matches)
+        if matches:
+            return matches # 未登录状态下是看不到的，需要Cookies
+        else:
+            return None
+    else:
+        return None
+
+
+
+
+def get_repo_url(repo_owner=None, repo_name=None, repo_url=None, repo_host="github.com"):
     # 如果repo_owner和repo_name均不为空，则构建repo_url
     if repo_owner and repo_name:
-        repo_url = "https://github.com/" + f"{repo_owner}/{repo_name}"
+        if repo_host=="github.com":
+            repo_url = "https://github.com/" + f"{repo_owner}/{repo_name}"
     else:
         repo_url = repo_url
     return repo_url
@@ -42,7 +76,22 @@ def test_get_repo_stars():
     # case construct
     print(get_repo_stars(repo_owner="uni-gal", repo_name="ayaka"))
     # case specific
-    print(get_repo_stars(repo_url="https://github.com/uni-gal/ayaka"))
+    # print(get_repo_stars(repo_url="https://github.com/uni-gal/ayaka"))
+
+def test_get_repo_watchers():
+    # 测试用例构造
+    print(get_repo_watchers(repo_owner="uni-gal", repo_name="ayaka"))
+    # 特定URL测试用例
+    # print(get_repo_watchers(repo_url="https://github.com/uni-gal/ayaka"))
+
+def test_get_repo_forks():
+    # 测试用例构造
+    print(get_repo_forks(repo_owner="uni-gal", repo_name="ayaka"))
+    # 特定URL测试用例
+    # print(get_repo_forks(repo_url="https://github.com/uni-gal/ayaka"))
+
 
 if __name__ == "__main__":
     test_get_repo_stars()
+    test_get_repo_watchers()
+    test_get_repo_forks()
